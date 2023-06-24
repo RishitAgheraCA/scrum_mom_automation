08728ed66132f73b552f42d160a28b8d5d2f310c
Saramsa <ssaramsa@gmail.com>
2023-06-24 14:18:19 -0400

    UI changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.static import static
 from django.urls import path
 from django.views.generic.base import RedirectView
 from dashboard.views import HomePageView, SubmitView
+from dashboard.views import HomePageView
+from dashboard.views import TaskPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    # path('submit-audio', SubmitView.as_view(), name='submit-audio'),
-
-
+    path('task',TaskPageView.as_view(),name='task'),
+    path('load',TaskPageView.as_view(),name='task_load')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -21,3 +23,21 @@ class SubmitView(View):
         import code
         code.interact(local=dict(globals(), **locals()))
         return render(request, 'dashboard/home_page.html')
+
+
+class TaskPageView(View):
+    def get(self, request):
+        return render(request, 'dashboard/tabulator.html')
+
+    def post(self,request):
+        if request.method == "POST" :
+            details =[
+                {"name":"BOB","task":"Write email series"},
+                {"name": "James","task":"Build Free trial Form"},
+                {"name": "Henry","task":"Write monthly Newsletter"},
+                {"name": "Raven","task":"Work on Home page"},
+                {"name": "Kathy","task":"make the layout responsive"},
+            ]
+            return JsonResponse({'details':details})
+        else:
+            return HttpResponse("No data found")
